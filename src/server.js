7807ad101549e069ed363ce8ead46eb86bfb2c5b
Alex Bessa <alexbessa.dev@gmail.com>
2026-02-05 19:40:04 -0300

    feat: create a basic web server

---
+++ src/server.js
@@ -0,0 +1,9 @@
+const http = require('node:http');
+
+http.createServer(function (request, response) {
+  response.writeHead(
+    200,
+    {'content-type': 'text/html; charset=utf-8'}
+  );
+  response.end('<h1>Hello Client!</h1>');
+}).listen(3000);
