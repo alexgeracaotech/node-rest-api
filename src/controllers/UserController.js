2b75f3889624260b97cb030c756f2fbf1c1a04a5
Alex Bessa <alexbessa.dev@gmail.com>
2026-02-05 21:53:56 -0300

    feat: create controller for users listing

---
+++ src/controllers/UserController.js
@@ -0,0 +1,11 @@
+const users = require('../mocks/users');
+
+module.exports = {
+  getUsers(response) {
+    response.writeHead(
+      200,
+      {'content-type': 'application/json'}
+    );
+    response.end(JSON.stringify(users));
+  }
+}
