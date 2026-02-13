078f698afa7e6e86284a06154df285690d1be554
Alex Bessa <alexbessa.dev@gmail.com>
2026-02-12 21:36:29 -0300

    feat: add module of url for request filter

--- src/server.js
+++ src/server.js
@@ -1,15 +1,20 @@
 import http from 'node:http';
+import url from 'node:url';
 
 import routes from './routes.js';
 import errors from './errors.js';
 
 http.createServer(function (request, response) {
+
+  const parsed_url = url.parse(request.url, true);
+
   const router = routes.find(function (route) {
-    return request.url === route.endpoint;
+    return parsed_url.pathname === route.endpoint;
   });
 
   if (router) {
-    router.handler(response);
+    request.query = parsed_url.query;
+    router.handler(request, response);
     return;
   }
 
