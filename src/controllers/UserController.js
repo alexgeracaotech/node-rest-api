702147bc3fdff2945042453c979bf367035daf5b
Alex Bessa <alexbessa.dev@gmail.com>
2026-02-12 21:38:43 -0300

    feat: add logical for order desc users

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,12 +1,21 @@
 import users from '../mocks/users.js';
 
 const UserController = {
-  index(response) {
+  index(request, response) {
+
+    const sorted_users = users.sort(function (a, b) {
+      if (request.query.order === 'desc') {
+        return b.id - a.id;
+      }
+
+      return a.id - b.id;
+    });
+
     response.writeHead(
       200,
       {'content-type': 'application/json'}
     );
-    response.end(JSON.stringify(users));
+    response.end(JSON.stringify(sorted_users));
   }
 }
 
